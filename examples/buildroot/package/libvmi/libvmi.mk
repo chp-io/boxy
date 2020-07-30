17485c5fe6c4b66b79699eb68e7f3344fec9c0f4
Christopher Pelloux <git@chp.io>
2020-07-30 19:23:46 -0400

    Buildroot: Add libvmi build options
    Facilitates development with the following build options:
    
    - BR2_LIBVMI_DEBUG enable libvmi debug definitions when defined
    - BR2_LIBVMI_PATH use local copy of libvmi directory when set

--- examples/buildroot/package/libvmi/libvmi.mk
+++ examples/buildroot/package/libvmi/libvmi.mk
@@ -4,9 +4,15 @@
 #
 ################################################################################
 
-LIBVMI_VERSION = 1dd5a2e48e43b70b50d7ff4a01ad1fcb3b8cbba7
-LIBVMI_SITE = $(call github,chp-io,libvmi,$(LIBVMI_VERSION))
-LIBVMI_REPO = https://github.com/chp-io/libvmi
+ifeq ($(BR2_LIBVMI_PATH),)
+	LIBVMI_VERSION = 1dd5a2e48e43b70b50d7ff4a01ad1fcb3b8cbba7
+	LIBVMI_SITE = $(call github,chp-io,libvmi,$(LIBVMI_VERSION))
+	LIBVMI_REPO = https://github.com/chp-io/libvmi
+else
+	LIBVMI_SITE = ${BR2_LIBVMI_PATH}
+	LIBVMI_SITE_METHOD = local
+	LIBVMI_OVERRIDE_SRCDIR_RSYNC_EXCLUSIONS = --exclude '/deps'
+endif
 
 LIBVMI_LICENSE = LGPLv3
 LIBVMI_LICENSE_FILES = COPYING
@@ -22,9 +28,13 @@ LIBVMI_CONF_OPTS = \
 	-DBUILD_EXAMPLES=ON \
 	-DENABLE_FREEBSD=OFF \
 	-DENABLE_STATIC=OFF \
-	-DENABLE_ADDRESS_CACHE=OFF \
-	-DENABLE_PAGE_CACHE=OFF \
-	-DCMAKE_BUILD_TYPE=DEBUG
-	# -DVMI_DEBUG=__VMI_DEBUG_ALL
+	-DENABLE_PAGE_CACHE=OFF
+
+ifeq ($(BR2_LIBVMI_DEBUG),)
+	LIBVMI_CONF_OPTS += -DCMAKE_BUILD_TYPE=Release
+else
+	LIBVMI_CONF_OPTS += -DCMAKE_BUILD_TYPE=Debug \
+						-DVMI_DEBUG=__VMI_DEBUG_ALL
+endif
 
 $(eval $(cmake-package))
